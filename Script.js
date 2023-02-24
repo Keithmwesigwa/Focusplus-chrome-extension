a54931407e79d66b04cdefcd945b5e0aa7a52015
Kevin Lule <66350935+Da-code135@users.noreply.github.com>
2023-02-24 16:09:54 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,26 +1,24 @@
 // define websites we have to block
-var blockedSites = [
-    
+var blockedSites = [];
 
-]
-//listen for webrequests and block them
-chrome.webRequest.OnBeforeRequest.addListener(
-    function(details){
-        for (var i = 0; i < blockedSites.length; i++){
-            if(details.url.indexOf(blockedSites[i])!=-1){
-                return {cancel:true};
+// listen for web requests and block them
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        for (var i = 0; i < blockedSites.length; i++) {
+            if (details.url.indexOf(blockedSites[i]) != -1) {
+                return { cancel: true };
             }
         }
     },
-    {urls:["<all_urls>"]},
+    { urls: ["<all_urls>"] },
     ["blocking"]
 );
-chrome.storage.onChanged.addListener(
-    function(changes, areaName){
-        if (areaName == "sync" && "blockedSites" in changes){
-            blockedSites = changes.blockedSites.newValue;
-        }   
-    }
-);
+
+// listen for changes to blocked sites and update the array
+chrome.storage.onChanged.addListener(function(changes, areaName) {
+    if (areaName === "sync" && "blockedSites" in changes) {
+        blockedSites = changes.blockedSites.newValue;
+    }   
+});
 
   
